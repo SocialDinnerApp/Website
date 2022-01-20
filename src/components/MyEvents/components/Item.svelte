6b133324d685db32d49dc2a10c02f579b8c4abea
NiclasCramer <niclascramer@gmail.com>
2022-01-20 22:23:56 +0100

    Niclas card adjust

--- src/components/MyEvents/components/Item.svelte
+++ src/components/MyEvents/components/Item.svelte
@@ -1,33 +1,35 @@
 <script>
-    export let item
-    import InnerTiles from "./InnerTiles.svelte";
+  export let item;
+  import InnerTiles from "./InnerTiles.svelte";
 </script>
-
-
 
 <!-- Erstellen der karte-->
 <div class="col d-flex justify-content-center">
-
   <div class="card ">
     <div class="row g-0">
       <div class="col-md-4">
-        <img src='images/dhbw.jpg' class="img-fluid rounded">
+        <img src="images/dhbw.jpg" class="img-fluid rounded" />
       </div>
-      <div class="col-md-8">
-        <div class="card-body">
-            <h5 class="card-title">{item.name}</h5>
-            <p class="card-text">{item.text}</p>
-            <InnerTiles bind:item/>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
 
+      <div class="col-md-8">
+        <div class="row g-0">
+          <div class="col-sm-1" />
+          <div class="col-md-11">
+            <div class="card-body">
+              <h5 class="card-title">{item.name}</h5>
+              <p class="card-text">{item.text}</p>
+              <InnerTiles bind:item />
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </div>
 
-
-  
 <style>
   .card {
     background-color: #fff;
@@ -36,8 +38,6 @@
     max-width: 1090px;
     margin: 30px 20px;
     position: center;
-    
-    
   }
   img {
     float: right;
@@ -46,6 +46,5 @@
     max-height: 280px;
     margin-top: 0px;
     margin-bottom: 0px;
-    
-}
+  }
 </style>
