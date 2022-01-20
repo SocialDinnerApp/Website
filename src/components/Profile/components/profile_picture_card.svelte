eef7a8fbd8bc1a35dbbb74a89875052ef0ef5e66
NiclasCramer <niclascramer@gmail.com>
2022-01-20 21:24:48 +0100

    Niclas Cramer Profilepage design overwork

--- src/components/Profile/components/profile_picture_card.svelte
+++ src/components/Profile/components/profile_picture_card.svelte
@@ -1,23 +1,26 @@
-<script lang='ts'>
-    //export let info 
-    import {info} from "./info"
-    console.log(info)
-   
-    const i =info[0]
-</script>
+<script lang="ts">
+    //export let info
+    import { info } from "./info";
+    console.log(info);
 
+    const i = info[0];
+</script>
 
 <div class="card ">
     <div class="card-body">
         <div class="d-flex flex-column align-items-center text-center">
-            <img src={i.image} alt="Admin" class="rounded-circle border border-light" width="340px"  >
+            <img
+                src={i.image}
+                alt="Admin"
+                class="rounded-circle border border-light"
+                width="340px"
+            />
         </div>
     </div>
 </div>
 
-
 <style>
-    .card{
+    .card {
         width: max-content;
         height: max-content;
         border: 0px;
--- src/components/Profile/profile.svelte
+++ src/components/Profile/profile.svelte
@@ -1,23 +1,17 @@
-<script lang='ts'>
-   import NavigationBar from "../NavigationBar/navigationbar.svelte";
-  import ProfilePictureCard from "./components/profile_picture_card.svelte";
-  import DataCard from "./components/data_card.svelte"
-import { info } from './components/info'
-import { push } from "svelte-spa-router";
+<script lang="ts">
+    import NavigationBar from "../NavigationBar/navigationbar.svelte";
+    import ProfilePictureCard from "./components/profile_picture_card.svelte";
+    import DataCard from "./components/data_card.svelte";
+    import { info } from "./components/info";
+    import { push } from "svelte-spa-router";
 
-    
-   
-    
-    
     function onClickTile() {
-        push('#/profile/edit');
-       // console.log('Ja')
+        push("#/profile/edit");
+        // console.log('Ja')
     }
- 
 </script>
 
 <NavigationBar route="/profile" />
-
 
 <div
     class="g-5 justify-content-evenly m-5 p-5 border border-secondary rounded shadow"
@@ -27,47 +21,48 @@ import { push } from "svelte-spa-router";
     </div>
     <div class="container">
         <div class="main-body">
-        
-
-        
-            <div class="row gutters-sm">
-                <div class="col-md-1 "> </div>
-                <div class="col-md-4 align-self-center " style="margin-top: 20px;">
-                      
-                      <ProfilePictureCard />
-                
+            <div class="row">
+                <div
+                    class="d-flex justify-content-center "
+                    style="margin-top: 20px;"
+                >
+                    <ProfilePictureCard />
                 </div>
-            
-                <div class="col-md-6 ">
-                  <div class="card mb-3">
-                      {#each info as info (info.id)}
-                      <DataCard {info}/>
-                      {/each}
-                  </div>
-
-                </div>
-                <div class="col-md-1 "> </div>
             </div>
-  
-          </div>
-          <div class="row">
+
+            <div class="row gutters-sm">
+                <div class="col-md-1 " />
+
+                <div class="col-md-10 ">
+                    <div class="align-items-center">
+                        <div class="card mb-3 " style="max-width: 900px;">
+                            {#each info as info (info.id)}
+                                <DataCard {info} />
+                            {/each}
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-1 " />
+            </div>
+        </div>
+        <div class="row">
             <div class="col-sm-12 d-flex justify-content-center">
-                <button on:click={onClickTile}
-                type="button"
-                class="btn btn-primary py-2 px-3 "
-                data-bs-toggle="modal"
-                data-bs-target="#exampleModa">
-                    Ändern 
+                <button
+                    on:click={onClickTile}
+                    type="button"
+                    class="btn btn-primary py-2 px-3 "
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModa"
+                >
+                    Ändern
                 </button>
             </div>
         </div>
-
-      </div>
+    </div>
 </div>
 
-
 <style>
-    .card{
+    .card {
         margin-top: 40px;
     }
 </style>
